ad3056fa39240363de886f056f0d37b3fe1afd38
Anton Zotov <votoznotna@gmail.com>
2024-04-19 18:38:45 -0700

    replaced context with ctx

--- routes/events.go
+++ routes/events.go
@@ -55,69 +55,69 @@ func createEvent(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
 
-func updateEvent(context *gin.Context) {
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+func updateEvent(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
 		return
 	}
 
-	userId := context.GetInt64("userId")
+	userId := ctx.GetInt64("userId")
 	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
 		return
 	}
 
 	if event.UserID != userId {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event."})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event."})
 		return
 	}
 
 	var updatedEvent models.Event
-	err = context.ShouldBindJSON(&updatedEvent)
+	err = ctx.ShouldBindJSON(&updatedEvent)
 
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
 
 	updatedEvent.ID = eventId
 	err = updatedEvent.Update()
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event."})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event."})
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{"message": "Event updated successfully!"})
+	ctx.JSON(http.StatusOK, gin.H{"message": "Event updated successfully!"})
 }
 
-func deleteEvent(context *gin.Context) {
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+func deleteEvent(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
 		return
 	}
 
-	userId := context.GetInt64("userId")
+	userId := ctx.GetInt64("userId")
 	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
 		return
 	}
 
 	if event.UserID != userId {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to delete event."})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to delete event."})
 		return
 	}
 
 	err = event.Delete()
 
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete the event."})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not delete the event."})
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully!"})
+	ctx.JSON(http.StatusOK, gin.H{"message": "Event deleted successfully!"})
 }
--- routes/users.go
+++ routes/users.go
@@ -8,50 +8,50 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func signup(context *gin.Context) {
+func signup(ctx *gin.Context) {
 	var user models.User
 
-	err := context.ShouldBindJSON(&user)
+	err := ctx.ShouldBindJSON(&user)
 
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
 
 	err = user.Save()
 
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user."})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user."})
 		return
 	}
 
-	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
+	ctx.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
 }
 
-func login(context *gin.Context) {
+func login(ctx *gin.Context) {
 	var user models.User
 
-	err := context.ShouldBindJSON(&user)
+	err := ctx.ShouldBindJSON(&user)
 
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
 
 	err = user.ValidateCredentials()
 
 	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Could not authenticate user."})
+		ctx.JSON(http.StatusUnauthorized, gin.H{"message": "Could not authenticate user."})
 		return
 	}
 
 	token, err := utils.GenerateToken(user.Email, user.ID)
 
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user."})
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user."})
 		return
 	}
 
-	// context.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful!", "token": token})
+	// ctx.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+	ctx.JSON(http.StatusOK, gin.H{"message": "Login successful!", "token": token})
 }
